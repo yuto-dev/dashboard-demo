29ad0b9c0a30674592ca61f51dc93f7b694d9177
Abiyyu Bagindo <directx178@gmail.com>
2024-06-27 09:46:54 +0700

    Added menu

--- server.js
+++ server.js
@@ -9,17 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/data', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM students');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-app.get('/kendaraan-data', async (req, res) => {
+app.get('/kendaraan-data2', async (req, res) => {
+  // 2. Data Jumlah Kendaraan Bermotor
   try {
     const result = await pool.query(`
       SELECT TipeKendaraan, COUNT(*) as count
@@ -32,6 +23,41 @@ app.get('/kendaraan-data', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+app.get('/kendaraan-data3', async (req, res) => {
+  // 3. Data Jumlah Kendaraan Bermotor Yang Tidak Membayar Pajak Selama 5 Tahun
+  try {
+    const result = await pool.query(`
+      SELECT TipeKendaraan, COUNT(*) as count
+      FROM kendaraanTable
+      WHERE TanggalRegistrasi < '2020-01-01'
+      AND TanggalRegistrasi > '2018-12-31'
+      GROUP BY TipeKendaraan;
+    `);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.get('/kendaraan-data4', async (req, res) => {
+  // 4. Data Jumlah Kendaraan Bermotor Yang Tidak Membayar Pajak Selama 7 Tahun
+  try {
+    const result = await pool.query(`
+      SELECT TipeKendaraan, COUNT(*) as count
+      FROM kendaraanTable
+      WHERE TanggalRegistrasi < '2018-01-01'
+      AND TanggalRegistrasi > '2016-12-31'
+      GROUP BY TipeKendaraan;
+    `);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
